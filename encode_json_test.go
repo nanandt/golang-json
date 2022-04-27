9fc483a9fb6e2178f60d409c1cf3f6687dfa9cb9
nanandt <rizkyfatih007@gmail.com>
2022-04-27 22:02:56 +0700

    Encode

--- encode_json_test.go
+++ encode_json_test.go
@@ -1,1 +1,22 @@
 package golang_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func logJson(data interface{}) {
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bytes))
+}
+
+func TestEncode(t *testing.T) {
+	logJson("rizky")
+	logJson(3)
+	logJson(true)
+	logJson([]string{"rizky", "ganteng"})
+}
