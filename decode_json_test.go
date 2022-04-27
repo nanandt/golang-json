6c5ced7cf11900a6cc4c456eb703c7be6276671f
nanandt <rizkyfatih007@gmail.com>
2022-04-27 22:03:14 +0700

    Decode

--- decode_json_test.go
+++ decode_json_test.go
@@ -1,1 +1,24 @@
 package golang_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestDecode(t *testing.T) {
+	jsonString := `{"FirstName":"rizky","MiddleName":"fatih","LastName":"slebew","Age":20,"Married":true}`
+	jsonBytes := []byte(jsonString)
+
+	customer := &Customer{}
+	err := json.Unmarshal(jsonBytes, customer)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(customer)
+	fmt.Println(customer.FirstName)
+	fmt.Println(customer.MiddleName)
+	fmt.Println(customer.Age)
+	fmt.Println(customer.Married)
+}
