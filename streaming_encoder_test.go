15100bdd7c8743b957d4d7ccc552965f5c045910
nanandt <rizkyfatih007@gmail.com>
2022-05-06 23:07:21 +0700

    Streaming Encoder

--- streaming_encoder_test.go
+++ streaming_encoder_test.go
@@ -1,1 +1,23 @@
 package golang_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestStreamingEncoder(t *testing.T) {
+	writer, _ := os.Create("CustomerOut.json")
+	encoder := json.NewEncoder(writer)
+
+	customer := Customer{
+		FirstName:  "Muhamad",
+		MiddleName: "Fatih",
+		LastName:   "Rizky",
+		Married:    false,
+	}
+
+	encoder.Encode(customer)
+	fmt.Println(customer)
+}
