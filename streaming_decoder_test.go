beff509e44e18c9d5344f486b870376f5035fc0f
nanandt <rizkyfatih007@gmail.com>
2022-05-06 23:07:04 +0700

    Streaming Decoder

--- streaming_decoder_test.go
+++ streaming_decoder_test.go
@@ -1,1 +1,18 @@
 package golang_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestStreamingDecoder(t *testing.T) {
+	reader, _ := os.Open("Customer.json")
+	decoder := json.NewDecoder(reader)
+
+	customer := &Customer{}
+	decoder.Decode(customer)
+
+	fmt.Println(customer)
+}
